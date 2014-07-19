a24bb316b3b19a4564a28ac58a0fdbc5d7d0fb36
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-19 16:09:29 -0400

    Added code to a method to `to_i` the factory quantity

--- lib/generator/factory.rb
+++ lib/generator/factory.rb
@@ -7,9 +7,15 @@ module Generator
     end
 
     def number(of_type = Fixnum, &b)
-      Generator::Integer.new(@quantity.is_a?(Symbol) ? 1 : @quantity, &b)
+      Generator::Integer.new(quantity_to_i, &b)
     end
 
     alias_method :numbers, :number
+
+    private
+
+    def quantity_to_i
+      @quantity.is_a?(Symbol) ? 1 : @quantity
+    end
   end
 end
