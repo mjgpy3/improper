f6c2703f7d498378894705a6e70e2d679d5dd862
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-19 17:21:18 -0400

    Added pending specs for more than one item, which will happen later

---
+++ spec/lib/generator/string_spec.rb
@@ -0,0 +1,29 @@
+require './lib/generator/string.rb'
+
+describe Generator::String do
+  let(:generator) { described_class.new(quantity) }
+
+  [2, 3, 1000, 3001].each do |q|
+    xcontext "when #{q} items are generated" do
+      let(:quantity) { q }
+
+      specify { expect { |b| described_class.new(quantity, &b) }.to yield_control.exactly(q).times }
+
+      [].each do |method_name|
+        describe "##{method_name}" do
+          specify { expect { |b| described_class.new(quantity).send(method_name, &b) }.to yield_control.exactly(q).times }
+        end
+      end
+
+      [].each do |method_name|
+        describe "##{method_name}" do
+          specify { expect { |b| described_class.new(quantity).send(method_name, 5, &b) }.to yield_control.exactly(q).times }
+        end
+      end
+    end
+  end
+
+  context 'when 1 item is generated' do
+    let(:quantity) { 1 }
+  end
+end
