fa85bda9ee1cfbcf01819996da6dc27d6de88c8d
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-19 15:29:02 -0400

    More refactoring of int generators

--- lib/generator/integer.rb
+++ lib/generator/integer.rb
@@ -2,27 +2,22 @@ module Generator
   class Integer
     ARBITRARY_UPPER_BOUND = 123459
     ARBITRARY_LOWER_BOUND = -123459
-    BOUNDS_ERROR = 'Lower bound cannot exeed upper'
-    EVEN_AND_ODD_ERROR = 'Cannot ensure even and odd'
-
-    METHOD_TITLE_TO_POSITION = {
-      'above' => 'lower',
-      'below' => 'upper',
-    }
+    ERROR_MESSAGE = 'Difficulty generating Integer. Please ensure properties are correct'
 
     def initialize(quantity)
       @quantity = quantity
       @lower_bound = ARBITRARY_LOWER_BOUND
       @upper_bound = ARBITRARY_UPPER_BOUND
-      @ensure_even = @ensure_odd = false
+      @conditions = [->(x){ !x.nil? }]
+      @depth_count = 0
       yield(generate_random) if block_given?
     end
 
     ['', 'and_'].each do |chain_prefix|
-      METHOD_TITLE_TO_POSITION.each do |place, position|
+      ['above', 'below'].each do |place|
         class_eval <<-BoundsMethods
         def #{chain_prefix}#{place}(bound)
-          @#{position}_bound = bound
+          @#{(place == 'above' ? 'lower' : 'upper')}_bound = bound
           yield(generate_random) if block_given?
           self
         end
@@ -33,7 +28,7 @@ module Generator
     ['even', 'odd'].each do |question|
       class_eval <<-QuestionMethods
       def that_is_#{question}
-        @ensure_#{question} = true
+        @conditions << ->(x){ x.#{question}? }
         yield(generate_random) if block_given?
         self
       end
@@ -42,25 +37,16 @@ module Generator
 
     private
 
-    def yield_or_self
-      block_given? ? yield(generate_random) : self
-    end
-
     def generate_random
-      guard_inconsistent_properties
+      fail ERROR_MESSAGE if @depth_count > 3000
+      @depth_count += 1
 
       a_fixnum = rand(@lower_bound..@upper_bound)
-      a_fixnum = generate_random if fails_property?(a_fixnum)
-      a_fixnum
+      meets_properties?(a_fixnum) ? a_fixnum : generate_random
     end
 
-    def fails_property?(number)
-      (@ensure_even && !number.even?) || (@ensure_odd && !number.odd?)
-    end
-
-    def guard_inconsistent_properties
-      raise BOUNDS_ERROR if @lower_bound > @upper_bound
-      raise EVEN_AND_ODD_ERROR if @ensure_even && @ensure_odd
+    def meets_properties?(number)
+      @conditions.all? { |c| c.(number) }
     end
   end
 end
